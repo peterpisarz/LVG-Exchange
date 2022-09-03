b936d3682fb379ade8288d0e14cd8b6dbf1bc3b7
Peter Pisarz <peter.pisarz@gmail.com>
2022-09-03 12:23:50 -0500

    7. Token Transfer Delegation

--- test/Token.js
+++ test/Token.js
@@ -94,7 +94,6 @@ describe("Token", ()=> {
 	describe('Approving Tokens', () => {
 		let amount, transaction, result
 
-
 		beforeEach(async () => {
 			amount = tokens(100)
 			transaction = await token.connect(deployer).approve(exchange.address, amount)
@@ -122,6 +121,49 @@ describe("Token", ()=> {
 
 			it('Rejects invalid spenders', async () => {
 				await expect(token.connect(deployer).approve("0x0000000000000000000000000000000000000000", amount)).to.be.reverted
+			})
+		})
+	})
+
+	describe('Delegated Token Transfers', () => {
+		let amount, transaction, result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+			beforeEach(async () => {
+				transaction = await token.connect(exchange).transferFrom(deployer.address, receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('Transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(ethers.utils.parseUnits("999900", "ether"))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
+
+			it('Resets the allowance', async () => {
+				expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0)
+			})
+			
+			it('Emits a Transfer event', async () => {
+				const event = result.events[0]
+				//console.log(event)
+				expect(event.event).to.equal('Transfer')
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+		        expect(args.to).to.equal(receiver.address)
+		        expect(args.value).to.equal(amount)
+			})
+		})
+		
+		describe("Failure", () => {
+			it('Rejects insufficient amounts', async () => {
+				const invalidAmount = tokens(100000000)
+				await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted
 			})
 		})
 	})
