57f70ed0869766426412946e5f322702a23da6c5
peterpisarz <peter.pisarz@gmail.com>
2023-04-19 18:04:04 -0500

    Adding Mumbai

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -12,17 +12,17 @@ async function main() {
   await lvg.deployed()
   console.log(`LVG Deployed to: ${lvg.address}`)
 
-  // const mETH = await Token.deploy('mETH', 'mETH', '1000000')
-  // await mETH.deployed()
-  // console.log(`mETH Deployed to: ${mETH.address}`)
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH Deployed to: ${mETH.address}`)
 
-  // const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
-  // await mDAI.deployed()
-  // console.log(`mDAI Deployed to: ${mDAI.address}`)
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI Deployed to: ${mDAI.address}`)
 
-  // const exchange = await Exchange.deploy(accounts[1].address, 10)
-  // await exchange.deployed()
-  // console.log(`Exchange Deployed to: ${exchange.address}`)
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
 main()
